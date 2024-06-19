26325c31b7fa7904c33fc35bac1e90e2dc7eac03
amritlshidling <123490014+amritlshidling@users.noreply.github.com>
2024-06-19 15:55:49 +0100

    Learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,8 +3,50 @@ import { renderPaymentSummary } from "./checkout/PaymentSummary.js";
 import { loadProducts } from "../data/products.js";
 // import '../data/cart-class.js'
 // import '../data/backend-practice.js'
+import { loadCart } from "../data/cart.js";
 
-loadProducts(()=>{
+Promise.all([
+  new Promise((resolve)=>{
+    loadProducts(()=>{
+      resolve('one');
+    });
+  }),
+  new Promise((resolve)=>{
+    loadCart(()=>{
+      resolve('two');
+    });
+  })
+]).then((value) => {
+  console.log(value);
   renderOrderSummary();
   renderPaymentSummary();
 });
+/*
+new Promise((resolve)=>{
+  loadProducts(()=>{
+    resolve('value1');
+  });
+ 
+}).then((value)=>{
+  return new Promise((resolve)=> {
+    loadCart(()=>{
+      console.log(value);
+      resolve('value2');
+      
+    });
+  });
+}).then((value)=>{
+  console.log(value);
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+
+*/
+/*
+loadProducts(() => {
+  loadCart(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+});
+*/
